44cc5476111a2ba88f70b3ab2abbac77a8072bce
gu <oncwnuBTdEtxgry3-_cnOHo4GA8s@git.weixin.qq.com>
2024-05-22 09:04:59 +0800

    first commit

---
+++ src/main/java/com/example/hellospringboot/domain/BaseEntity.java
@@ -0,0 +1,7 @@
+package com.example.hellospringboot.domain;/**
+ * BaseEntity class
+ *
+ * @author gukt
+ * @since 1.0
+ */public class BaseEntity {
+}
---
+++ src/main/java/com/example/hellospringboot/domain/User.java
@@ -0,0 +1,7 @@
+package com.example.hellospringboot.domain;/**
+ * User class
+ *
+ * @author gukt
+ * @since 1.0
+ */public class User {
+}
