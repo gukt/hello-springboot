c817921eecab11d77992f329e95f1f7f9ea9d010
gu <oncwnuBTdEtxgry3-_cnOHo4GA8s@git.weixin.qq.com>
2024-05-22 09:10:05 +0800

    misc updates

--- src/main/java/com/example/hellospringboot/domain/BaseEntity.java
+++ src/main/java/com/example/hellospringboot/domain/BaseEntity.java
@@ -1,7 +1,29 @@
-package com.example.hellospringboot.domain;/**
+package com.example.hellospringboot.domain;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+import java.io.Serializable;
+
+/**
  * BaseEntity class
  *
  * @author gukt
  * @since 1.0
- */public class BaseEntity {
+ */
+@MappedSuperclass
+@Getter
+@Setter
+@SuperBuilder
+public abstract class BaseEntity<T extends Serializable> {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private T id;
 }
+
--- src/main/java/com/example/hellospringboot/domain/User.java
+++ src/main/java/com/example/hellospringboot/domain/User.java
@@ -1,7 +1,23 @@
-package com.example.hellospringboot.domain;/**
+package com.example.hellospringboot.domain;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+/**
  * User class
  *
  * @author gukt
  * @since 1.0
- */public class User {
+ */
+@Entity
+@Table(name = "t_users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 }
